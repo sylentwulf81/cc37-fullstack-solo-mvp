f9f11e94d7687f2c311f6e36a03e9aa4540b26cf
sylentwulf81 <dlavizzo@gmail.com>
2025-02-17 15:16:31 +0900

    fix: revert previous commit and fix AWS secret error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         <div className="front-page-container">
           <h1 className="front-page-title">You Craft Your Story. ScriptHero Does The Rest.</h1>
           <h2 className="front-page-subtitle">ScriptHero is perfect tool to write perfectly formatted, Western-style comic books.</h2>
-          <p><img src="https://picsum.photos/id/1/800/600" /></p>
+          <p><img src="https://picsum.photos/id/1/800/600" alt={"placeholder for main page hero image"} /></p>
           <h3 className="front-page-body">Placeholder for LOGIN big button</h3>
           <h3 className="front-page-body">Placeholder for REGISTER big button</h3>
         </div>
