5133f11ef67f391c2c5080767ab4929843a2f8c9
sylentwulf81 <dlavizzo@gmail.com>
2025-02-21 09:07:34 +0900

    fix: updates app-level documentation

--- client/src/components/ModalComponents/ModalSubComponents/03_Help/Documentation.tsx
+++ client/src/components/ModalComponents/ModalSubComponents/03_Help/Documentation.tsx
@@ -1,8 +1,33 @@
 // styling
-
 import "../../../_root.css"; // global styling
 import "../../Modal.css"; // global modal styling
 
 export default function FAQ() {
-  return <div>Documentation will go here</div>;
+  return (
+    <>
+      <h1 className="header">How To Use ScriptHERO</h1>
+
+      <h2 className="subheader">Parts of a Script</h2>
+
+      <div className="script-element">
+        <strong>Page</strong> | <code>page #</code>
+      </div>
+
+      <div className="script-element">
+        <strong>Panel</strong> | <code>panel #</code>
+      </div>
+
+      <div className="script-element">
+        <strong>Dialogue</strong> | <code>charactername:</code>
+      </div>
+
+      <div className="script-element">
+        <strong>Sound effects</strong> | <code>sfx:</code>
+      </div>
+
+      <div className="script-element">
+        <strong>Captions</strong> | <code>caption:</code>
+      </div>
+    </>
+  );
 }
